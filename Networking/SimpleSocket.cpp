07e81cc4e12cdb8c9bf1f6f371e32b9472d6bc9d
praneet <praneet18.ark@gmail.com>
2025-07-04 02:03:11 +0530

    first commit

---
+++ Networking/SimpleSocket.cpp
@@ -0,0 +1,46 @@
+#include "SimpleSocket.hpp"
+
+HDE::SimpleSocket:SimpleSocket(int domain, int service, int protocol,
+     int port, u_long interface, ) 
+{
+    //defining address structure
+    address.sin_family = domain;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = htonl(interface);
+    //establish socket
+    sock = socket(domain, service, protocol);
+    test_connection(sock);
+    //establish connection
+    connection = connect_to_network(sock, address);
+    test_connection(connection);
+}
+
+//test connection virtual function
+void HDE::SimpleSocket::test_connection(int item_to_test) 
+{
+    //confirm that the sokcet or connecion is properly established
+    if(item_to_test < 0) 
+    {
+        perror("Failed to connect!...");
+        exit(EXIT_FAILURE);
+    }
+}
+
+//Getter fucntions
+
+struct sockaddr_in HDE::SimpleSocket::get_address()
+{
+    return address;
+}
+
+int HDE::SimpleSocket::get_sock()
+{
+    return sock;
+}
+
+int HDE::SimpleSocket::get_connection()
+{
+    return connection;
+}
+
+
---
+++ Networking/SimpleSocket.hpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+
+namespace HDE
+{
+class SimpleSocket
+{
+private:
+    struct sockaddr_in address;  
+    int sock;
+    int connection;
+
+public:
+//constructor
+    SimpleSocket(int domain, int service, int protocol, int port, u_long interface);
+    //virtual func to connect to network
+    virtual void connect_to_network(int sock, struct sockaddr_in address) = 0;
+    //func to test sockets and connections
+    void test_connection(int);
+    //getter funcs
+    struct sockaddr_in get_address();
+    int get_sock();
+    int get_connection();     
+};
+}
